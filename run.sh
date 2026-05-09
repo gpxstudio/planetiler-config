6c946756011cb5ba46760ae74b9c736e42621b5b
vcoppe <vianney.coppe@gmail.com>
2026-05-09 17:09:10 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p data
+
+docker run \
+    -e JAVA_TOOL_OPTIONS="-Xmx1g -XX:MaxHeapFreeRatio=40" \
+    -v "$(pwd)/data":/data \
+    ghcr.io/onthegomap/planetiler:latest \
+    --download --area=planet --bounds=planet \
+    --download-threads=10 --download-chunk-size-mb=1000 \
+    --fetch-wikidata \
+    --output=data/output.pmtiles \
+    --nodemap-type=array --storage=ram
